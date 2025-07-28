7fc889b5c6fe4a887c47ccd8e89b26ba5f0d68c9
niharika1112 <109222572+niharika1112@users.noreply.github.com>
2025-07-28 14:18:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,5 +31,6 @@ app.post("/leetcode", async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
